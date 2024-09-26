2e8991926133a6d150a193972a63f21bd3cc0679
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-26 12:37:59 +0100

    Day1

--- Day1/Boucles/challenge8.c
+++ Day1/Boucles/challenge8.c
@@ -4,21 +4,21 @@ int main(){
 
     int i, n;
 
-    int t1 = 0, t2 = 1;
+    int f0 = 0, f1 = 1;
 
-    int next = t1 + t2;
+    long long next = f0 + f1;
 
     printf("\nentrez le nombre de la Suite de Fibonacci :");
     scanf("%d", &n);
 
-    printf("\nla Suite de Fibonacci est : %d, %d, ", t1, t2);
+    printf("\nla Suite de Fibonacci est : %d, %d, ", f0, f1);
 
     for ( i = 3; i <= n; i++)
     {
         printf("%d, ", next);
-        t1 = t2;
-        t2 = next;
-        next = t1 + t2;
+        f0 = f1;
+        f1 = next;
+        next = f0 + f1;
     }
     
     return 0;
