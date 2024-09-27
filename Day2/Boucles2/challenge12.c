40e8416b113dd91c9ce8e0fd16a92a46078b4197
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-27 10:19:52 +0100

    day2

--- Day2/Boucles2/challenge12.c
+++ Day2/Boucles2/challenge12.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+void sort(int num[], int n){
+    int i,j;
+
+    for (i = 0; i < n-1; i++){
+        for (j = 0; j < n-i-1; i++){
+            if (num[j] > num[j+1]){   
+                int temp = num[j];
+                num[j] = num[j+1];
+                num[j+1] = temp;
+            }
+        }
+    }
+
+}
+
 int main() {
     int n;
 
@@ -11,6 +26,7 @@ int main() {
     for (int i =0; i<n;i++){
     printf("Entrez une chiffre %d : ", (i+1));
     scanf("%d", &num[i]); }
+
     
 
     return 0;
