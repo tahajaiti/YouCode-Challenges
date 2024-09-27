426b4db7a4497a788fb5fb52a294239359594ded
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-27 09:52:53 +0100

    day2

--- Day2/Boucles2/challenge10.c
+++ Day2/Boucles2/challenge10.c
@@ -3,12 +3,32 @@
 #include <stdlib.h> 
 #include <time.h> 
 
+char passgen(int n){
+
+    char storage[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$^&*()";
+    char password[n+1];
+   
+    srand(time(NULL));
+
+    for(int i = 0; i < n; i++) {
+      int index = rand() % (sizeof(storage) - 1);
+      password[i] = storage[index];
+    }
+
+    password[n] = '\0';
+
+    printf("\nle mot de pass est:");
+    printf("\n%s", password);
+}
+
+
 int main(){
     int n;
 
     printf("\nentrer un nombre: ");
     scanf("%d", &n);
 
+    passgen(n);
 
     return 0;
 }
