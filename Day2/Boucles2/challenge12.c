2b2488102cf3e8e6a295ae66c6ca016fd6369033
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-27 10:13:58 +0100

    Day2

---
+++ Day2/Boucles2/challenge12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("\ncombien de chiffres vas-tu saisir: ");
+    scanf("%d", &n);
+
+    int num[n];
+
+    for (int i =0; i<n;i++){
+    printf("Entrez une chiffre %d : ", (i+1));
+    scanf("%d", &num[i]); }
+    
+
+    return 0;
+}
