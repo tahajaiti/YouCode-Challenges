4f24837d59ce4b7d977de5fda68c5e4d04901059
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-28 12:48:02 +0100

    condition1

--- Day1/Conditions1/challenge12.c
+++ Day1/Conditions1/challenge12.c
@@ -1,39 +1,19 @@
 #include <stdio.h>
 
-typedef struct {int heures; int minutes; int secondes;} time;
+int main() {
+    int h1, m1, s1, h2, m2, s2;
 
-time read(){
-    time i;
-    scanf("%d:%d:%d", &i.heures, &i.minutes, &i.secondes);
-    return i;
-}
+    printf("Entrez le premier instant (HH:MM:SS) : ");
+    scanf("%d:%d:%d", &h1, &m1, &s1);
+    printf("Entrez le deuxieme instant (HH:MM:SS) : ");
+    scanf("%d:%d:%d", &h2, &m2, &s2);
 
-int compare(time t1, time t2){
-    if (t1.heures != t2.heures){
-        return t1.heures - t2.heures;
-    }
-    if (t1.minutes != t2.minutes){
-        return t1.minutes - t2.minutes;
-    }
-    return t1.secondes - t2.secondes;
-}
-
-int main(){
-
-    printf("\nentrez le premier instant [HH:MM:SS]: ");
-    time time1 = read();
-
-    printf("\nentrez le deuxieme instant [HH:MM:SS]: ");
-    time time2 = read();
-
-    int comparaison = compare(time1, time2);
-
-    if (comparaison < 0) {
-        printf("le premier instant vient avant le deuxième\n");
-    } else if (comparaison > 0) {
-        printf("le deuxième instant vient avant le premier\n");
+    if (h1 < h2 || (h1 == h2 && m1 < m2) || (h1 == h2 && m1 == m2 && s1 < s2)) {
+        printf("Le premier instant vient avant le deuxieme.\n");
+    } else if (h1 > h2 || (h1 == h2 && m1 > m2) || (h1 == h2 && m1 == m2 && s1 > s2)) {
+        printf("Le deuxieme instant vient avant le premier.\n");
     } else {
-        printf("il s'agit du même instant\n");
+        printf("Il sagit du même instant.\n");
     }
 
     return 0;
