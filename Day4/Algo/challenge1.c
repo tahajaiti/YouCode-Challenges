2b8c8130afdcce75f97d8a38578e1f9ee76dd00e
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-30 16:11:14 +0100

    challenges

--- Day4/Algo/challenge1.c
+++ Day4/Algo/challenge1.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 void swap(int *a, int *b){
-    int temp = a;
-    a = b;
-    b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
+void swap(int tab[],int size){
+    int i,j,sp;
+    
 
+
+}
 
 int main(){
     int tab[] = {1,5,-4,3,34,23,7};
