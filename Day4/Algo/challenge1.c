a7b8fca7e0c2b7b99741c45ccb8db12327fd121e
Taha Jaiti <kyojin.rar@gmail.com>
2024-09-30 16:07:38 +0100

    challenges

--- Day4/Algo/challenge1.c
+++ Day4/Algo/challenge1.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 
+void swap(int *a, int *b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
+
+
+int main(){
+    int tab[] = {1,5,-4,3,34,23,7};
+    int size = sizeof(tab) / sizeof(tab[0]);
+
+
+
+
+    return 0;
+}
