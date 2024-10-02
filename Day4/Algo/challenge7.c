9acce5a9a0af919ef112187cf04056eff2d81901
Taha Jaiti <kyojin.rar@gmail.com>
2024-10-02 09:36:43 +0100

    challenge

---
+++ Day4/Algo/challenge7.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int compare (const void* a, const void* b){
+    return (*(int*)a- *(int*)b);
+}
+
+bool searchb(int tab[], int start, int fin, int var) {
+    while (start <= fin) {
+        int milieu = start + (fin - start) / 2;
+
+        if (tab[milieu] == var)
+            return true;
+
+        if (tab[milieu] < var)
+            start = milieu + 1;
+
+        else
+            fin = milieu - 1;
+    }
+    return false;
+}
+
+int main(){
+    int tab[] = {2,5,42,23,14,9,7,29,17};
+    int size = sizeof(tab)/sizeof(tab[0]);
+
+    qsort(tab, size, sizeof(int), compare);
+
+    for(int i= 0; i < size; i++){
+        printf("%d, ", tab[i]);
+    }
+    printf("\n");
+
+    int search = 14;
+    if (searchb(tab, 0, size-1, search)){
+        printf("\n%d existe\n", search);}
+    else {printf("element existe pas\n");}
+
+    return 0;
+}
